b96f3be13e16a75c1eb6479e7e403fa71def2bf6
pavel <p.kolosov@list.ru>
2024-11-21 20:34:05 +0300

    up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.robokassa_sample"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
